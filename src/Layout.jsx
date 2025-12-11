b76609097510025c2cf596c2a8a47a59a3babd0e
Emanoel Silva de Amorim <emanoel.s.amorim@gmail.com>
2025-12-11 20:12:30 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { Button } from '@/components/ui/button';
 import { base44 } from '@/api/base44Client';
 import { createPageUrl } from '@/utils';
